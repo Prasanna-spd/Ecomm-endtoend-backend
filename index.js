ecb6d27e57d8b57912d815bbd0ed2b2354072a96
Sarada Prasanna Dash <dashsharada@gmail.com>
2024-06-20 23:10:03 +0530

    new test cors

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ server.use(
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ecomm-frontend-xi.vercel.app'); // Replace with your frontend URL
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
